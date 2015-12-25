071dbf4351b81146575fff98994e4c8b36c8968e
Dušan Jovanović <jovanovic.dusan@yahoo.com>
2015-12-25 04:10:57 +0100

    reducersalmax

---
+++ reduce_sale_max.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Reducer file
+"""
+
+import sys
+
+salesMax = 0
+oldKey = None
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    thisKey, thisSale = data
+    if oldKey and oldKey != thisKey:
+        print oldKey, "\t", salesMax
+        oldKey = thisKey
+        salesMax = 0
+
+    oldKey = thisKey
+    if thisSale > salesMax:
+        salesMax = thisSale
+
+if oldKey != None:
+    print oldKey, "\t", salesMax
