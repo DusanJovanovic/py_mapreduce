f2dac6a4a858305ce228d32c9667207b3e7d2939
Dušan Jovanović <jovanovic.dusan@yahoo.com>
2015-12-25 03:37:40 +0100

    added mforitems

---
+++ maper_for_items.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+Mapper for a Udacity Hadoop course
+"""
+
+import sys
+
+for line in sys.stdin:
+        # strip off extra whitespace, split on tab and put the data in an array
+        data = line.strip().split("\t")
+        if len(data) == 6:
+            date, time, store, item, cost, payment = data
+        # Now print out the data that will be passed to the reducer
+            if item in ["Toys", "Consumer Electronics"]:
+            print "{0}\t{1}".format(item, cost)
