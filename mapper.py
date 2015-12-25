81bea6ccd3f69b43c789b84ad7e81b4c7a93d042
Dušan Jovanović <jovanovic.dusan@yahoo.com>
2015-12-25 01:25:37 +0100

    Finished reducer

--- mapper.py
+++ mapper.py
@@ -2,3 +2,12 @@
 Mapper for a Udacity Hadoop course
 """
 
+import sys
+
+for line in sys.stdin:
+        # strip off extra whitespace, split on tab and put the data in an array
+        data = line.strip().split("\t")
+        if len(data) == 6:
+            date, time, store, item, cost, payment = data
+        # Now print out the data that will be passed to the reducer
+            print "{0}\t{1}".format(store, cost)
