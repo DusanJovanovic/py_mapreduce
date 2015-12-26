bca588a339bc49e2fba0b35968c7476eed6fa5ce
Dušan Jovanović <jovanovic.dusan@yahoo.com>
2015-12-26 03:54:39 +0100

    added mapper for html

---
+++ mapper_html.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Mapper for a Udacity Hadoop course
+"""
+
+import sys
+
+for line in sys.stdin:
+    # strip off extra whitespace, split on tab and put the data in an array
+    data = line.strip().split(" ")
+    if len(data) == 10:
+        ip, client_id, client_uname, date_time, time_zone, get, path, req, status, size = data
+    # Now print out the data that will be passed to the reducer
+        print "{0}\t{1}".format(ip, path)
+
+##with open("pypy.txt", "r") as infile:
+##    for line in infile:
+##        data = line.strip().split(" ")
+##        print data
